f7794b030d511d65e8e872a35b18bf8dd3bb5bbe
Frank Camilleri <fcamilleri22@gmail.com>
2016-11-19 13:41:57 -0500

    Now serving test main html sucessfully

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ var http = require("http");
 
 var file = new nodeStatic.Server("./static", {cache:0});
 
-http.createServer((req, res) => {
-    console.log(`Serving ${req.url}`);
+var handlePath = (path, req, res) => {
+    console.log(`Handling: ${path}`);
+    if (path == "/") {
+        path = "app/html/app.html";
+    }
+    console.log(`Serving: ${path}`)
+    req.url = path;
     file.serve(req, res);
+};
+
+http.createServer((req, res) => {
+    // console.log(`Serving ${req.url}`);
+    handlePath (req.url, req, res);
 }).listen(80, (err) => {if (err) throw err;});
