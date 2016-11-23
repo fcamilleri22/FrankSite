c259e90d89567fbc85f5b4f0c3e664d599b41066
Frank Camilleri <fcamilleri22@gmail.com>
2016-11-22 20:32:43 -0500

    Added html/css renderer

--- static/app/js/renderer.js
+++ static/app/js/renderer.js
@@ -1,14 +1,20 @@
-define(["jquery","require"],
-function($, require){
+define(["jquery","require", "app/cssHandler"],
+function($, require, cssHandler){
     //return function(containerDiv){
     exports = {};
 
-    exports.renderText = function(requireJsPrefix, dir, fileName, containerDiv){
+    var renderText = exports.renderText = function(requireJsPrefix, dir, fileName, containerDiv){
         return new Promise((resolve) => {
             require([`${requireJsPrefix}${dir}/${fileName}`], (text) => {
                 resolve(containerDiv.html(text));
             });
         });
+    };
+
+    //requires that HTML and CSS have the same name
+    exports.renderHtmlWithCss = function(name, containerDiv, isPermanent){
+        isPermanent ? cssHandler.load(name) : cssHandler.loadDynamic(name);
+        return renderText("text!", "html", `${name}.html`, containerDiv);
     };
 
     return exports;
