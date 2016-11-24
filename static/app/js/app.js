6121f6f3b4a38e2c2ab6e91ee542389948767b31
Frank Camilleri <fcamilleri22@gmail.com>
2016-11-23 20:20:26 -0500

    app.js now renders the appLanding

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -1,24 +1,7 @@
-define(["jquery", "app/renderer"],
-function($, renderer){
-    var htmlRoot = $("#rootWrapper");
+define(["jquery", "app/renderer", "app/util"],
+function($, renderer, util){
+    var root = $("#root");
     //htmlRoot.click( ()=> htmlRoot.append(" more tests"));
-    renderer.renderText("text!","html","test.html", htmlRoot).then(function(){
-        delayTest(5000).then(function(){
-            console.log("First fire!");
-            renderer.renderText("text!","html","test.html", $("#test2")).then(function(){
-                delayTest(5000).then(function(){
-                    console.log("WOOOOOO");
-                    htmlRoot.append("woo");
-                });
-            });
-        });
-    });
-
-    var delayTest = function(msDelay){
-        var delayP = new Promise((resolve) => {
-            setTimeout(resolve, msDelay);
-        });
-        return delayP;
-    };
+    renderer.renderHtmlWithCss("appLanding", root, true);
 
 });
