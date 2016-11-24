f1822c442b8e040083415ad8903cd6482bd35bbd
Frank Camilleri <fcamilleri22@gmail.com>
2016-11-23 20:19:52 -0500

    Use minified jquery

--- static/app/js/requirejsConfig.js
+++ static/app/js/requirejsConfig.js
@@ -3,7 +3,7 @@ require.config({
     paths: {
         app: "app/js",
         html: "app/html",
-        jquery: "lib/js/jquery/dist/jquery",
+        jquery: "lib/js/jquery/dist/jquery.min",
         requirejs: "lib/js/requirejs/require",
         text: "lib/js/text/text"
     }
