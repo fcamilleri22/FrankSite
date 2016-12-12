ca0d2f637f6887676513fffabeafefb21929bef6
Frank Camilleri <fcamilleri22@gmail.com>
2016-12-11 19:48:29 -0500

    JQuery? Whats that?!

--- static/app/js/renderer.js
+++ static/app/js/renderer.js
@@ -1,20 +1,20 @@
-define(["jquery","require", "app/cssHandler"],
-function($, require, cssHandler){
+define(["require", "app/cssHandler"],
+function(require, cssHandler){
     //return function(containerDiv){
     exports = {};
 
-    var renderText = exports.renderText = function(requireJsPrefix, dir, fileName, containerDiv){
+    var renderText = exports.renderText = function(requireJsPrefix, dir, fileName, documentNode){
         return new Promise((resolve) => {
             require([`${requireJsPrefix}${dir}/${fileName}`], (text) => {
-                resolve(containerDiv.html(text));
+                resolve(documentNode.innerHTML = text);
             });
         });
     };
 
     //requires that HTML and CSS have the same name
-    exports.renderHtmlWithCss = function(name, containerDiv, isPermanent){
+    exports.renderPage = function(name, documentNode, isPermanent){
         isPermanent ? cssHandler.load(name) : cssHandler.load(name, true);
-        return renderText("text!", "html", `${name}.html`, containerDiv);
+        return renderText("text!", "html", `${name}.html`, documentNode);
     };
 
     return exports;
