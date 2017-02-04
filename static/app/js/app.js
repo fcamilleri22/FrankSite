481bb142050154ce035049c58c54f7772d60eeb8
Frank Camilleri <fcamilleri22@gmail.com>
2017-02-04 16:27:54 -0500

    Paragraphs abound!

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -1,5 +1,6 @@
-define(["app/components/DisplayGrid", "app/components/PageTitle", "app/util/HTMLFragmentBuilder"],
-function(DisplayGrid, PageTitle, h){
+define(["app/components/DisplayGrid", "app/components/PageTitle", "app/components/ParagraphSection",
+    "app/util/HTMLFragmentBuilder"],
+function(DisplayGrid, PageTitle, ParagraphSection, h){
 
     document.body.style.margin = "0px";
     document.body.style["background-color"] = "#333333";
@@ -41,12 +42,34 @@ function(DisplayGrid, PageTitle, h){
     ];
 
     const titleContainer = h.div({"style":{"background-color":"#333333"}});
-    const gridContainer = h.div({"style":{"background-color":"#999999"}});
+    const paragraphContainer = h.div({"style":{"background-color":"#999999"}});
+    const gridContainer = h.div({"style":{"background-color":"#333333"}});
+    const footerContainer = h.div({"style":{"background-color":"#999999"}});
     const title = new PageTitle("Frank Camilleri","Full Stack Web Engineer, DevOps Professional", titleContainer);
+    const paragraph = new ParagraphSection(paragraphContainer);
     const grid = new DisplayGrid(items, gridContainer);
-    Promise.all([title.render(), grid.render()]).then(()=>{
+    paragraph.addContent(
+        `I am an <b><em>enthusiastic, passionate, and diligent</b></em> engineer with a <u><b><em>proven</u></b></em>
+        ability to bring people together, orchestrate work for an <b><em>optimal return on your investment</b></em>,
+        and ultimately, get your <b><em>ideas to market.</b></em>`,
+        "Don't just bring in any 'computer guy' from the backwoods of the internet..."
+    );
+    paragraph.addContent(
+        `I prefer to solve technology problems from a <b><em>holistic and generalized</b></em> point of view, whether its a shoddily
+        performing back end/API, a sluggish front end experience, a discombobulated database schema, lousy hardware/cloud
+        configurations, <b><em>or a complete lack of any technology stack whatsoever.</b></em>`
+    );
+    paragraph.addContent(
+        `With my ability to <b><em>organize and automate</b></em> vital development processes such as deployment and testing,
+        I can even help <b><em>clear up costly bottlenecks</b></em> in your team's operating procedures that <b><em>hinder your
+        organization's ability</b></em> to release new functionality quickly, preventing your team from resorting
+        to risky heroics, technical debt inducing hacks, and demoralizing long hours <b><em>to accomplish your business goals.</b></em></p>`
+    );
+    Promise.all([title.render(), grid.render(), paragraph.render()]).then(()=>{
         let frag = document.createDocumentFragment();
         frag.appendChild(titleContainer);
+        frag.appendChild(paragraphContainer);
+        frag.appendChild(footerContainer);
         frag.appendChild(gridContainer);
         document.body.appendChild(frag);
     });
---
+++ static/app/js/components/ParagraphSection.js
@@ -0,0 +1,101 @@
+define(["app/util/HTMLFragmentBuilder"],
+(h) => {
+    return class ParagraphSection {
+        constructor(container){
+            this.container = container;
+            this.titles = [];
+            this.contents = [];
+            this.boldStyle = {color:"#0088cc"};
+            this.emphasisStyle = {};
+            this.underlineStyle = {};
+            this.component;
+        }
+
+        buildTitle(title){
+            let formattedContent = this.addStyleToBoldText(title);
+            formattedContent = this.addStyleToEmphasisText(formattedContent);
+            formattedContent = this.addStyleToUnderlinedText(formattedContent);
+            return h.h2({
+                class: "paragraph-title",
+                style: {
+
+                }
+            }, formattedContent);
+        }
+
+        buildContent(content){
+            let formattedContent = this.addStyleToBoldText(content);
+            formattedContent = this.addStyleToEmphasisText(formattedContent);
+            formattedContent = this.addStyleToUnderlinedText(formattedContent);
+            return h.p({
+                class: "paragraph",
+                style: {
+                    "em":{color:"#123123"}
+                }
+            }, formattedContent);
+        }
+
+        addStyleToBoldText(text){
+            let htmlReplace = `<b style="`
+            Object.keys(this.boldStyle).forEach(styleProp =>{
+                htmlReplace = `${htmlReplace} ${styleProp}: `+this.boldStyle[styleProp]+";";
+            })
+            return text.replace(/<b>/g, htmlReplace+`">`);
+        }
+
+        addStyleToEmphasisText(text){
+            let htmlReplace = `<em style="`
+            Object.keys(this.emphasisStyle).forEach(styleProp =>{
+                htmlReplace = `${htmlReplace} ${styleProp}: `+this.emphasisStyle[styleProp]+";";
+            })
+            return text.replace(/<em>/g, htmlReplace+`">`);
+        }
+
+        addStyleToUnderlinedText(text){
+            let htmlReplace = `<u style="`
+            Object.keys(this.underlineStyle).forEach(styleProp =>{
+                htmlReplace = `${htmlReplace} ${styleProp}: `+this.underlineStyle[styleProp]+";";
+            })
+            return text.replace(/<u>/g, htmlReplace+`">`);
+        }
+
+        addContent(paragraph, title){
+            this.contents.push(paragraph ? paragraph : "");
+            this.titles.push(title ? title : "");
+        }
+
+        buildSectionContent(){
+            let itr = 0;
+            let retVal = [];
+            while (itr != this.contents.length){
+                if (this.titles[itr] != "") retVal.push(this.buildTitle(this.titles[itr]));
+                if (this.contents[itr] != "") retVal.push(this.buildContent(this.contents[itr]));
+                ++itr;
+            }
+            return retVal;
+        }
+
+        buildComponent(){
+            return h.div({
+                class: "Paragraph",
+                style: {
+                    padding:"1em",
+                    margin:"auto",
+                    "text-align": "left",
+                    width: "80%"
+                }
+            },
+            ...this.buildSectionContent());
+        }
+
+        render(){
+            return new Promise(resolve => {
+                this.component = this.buildComponent();
+                this.container.innerHTML = "";
+                this.container.appendChild(this.component);
+                resolve(this);
+            });
+        }
+    };
+
+});
