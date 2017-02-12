3b31dc4ea226c5f525b9d299eac02e040603d23c
Frank Camilleri <fcamilleri22@gmail.com>
2017-02-11 20:07:39 -0500

    Added overridden render function

--- static/app/js/components/LinkButton.js
+++ static/app/js/components/LinkButton.js
@@ -1,4 +1,42 @@
 define(["app/util/HTMLFragmentBuilder", "app/components/Button"],
 (h, Button) => {
+    return class LinkButton extends Button {
+        constructor(label, container, link, style, hoverStyle){
+            super(label, container, style, hoverStyle);
+            this.link = link;
+        }
 
+        //overridden Render
+        render(){
+            const thisClass = this;
+
+            let buttonDiv =
+            h.div({
+                className: "button",
+                style: this.style,
+                onmouseover:    () => {
+                    this.mouseover = true;
+                    this.applyStyle(thisClass.hoverStyle);
+                },
+                onmouseleave:   () => {
+                    this.mouseover = false;
+                    if (this.active == false) this.applyStyle(thisClass.style);
+                },
+            }, this.label);
+
+            this.component =
+            h.a({
+                href: this.link
+            }, buttonDiv);
+
+            this.container.innerHTML = "";
+            this.container.appendChild(this.component);
+            return this;
+        }
+
+        update(){
+            super.update();
+            this.component.href = this.link;
+        }
+    };
 });
