b321b4e72a01405061ccb6fe46802ed526564d9b
Frank Camilleri <fcamilleri22@gmail.com>
2017-02-12 18:57:32 -0500

    WIP. You broke the grid, man

--- static/app/js/components/DisplayGridRow.js
+++ static/app/js/components/DisplayGridRow.js
@@ -1,37 +1,31 @@
-define(["app/util/HTMLFragmentBuilder", "app/components/Button"],
-(h, Button) => {
+define(["app/util/HTMLFragmentBuilder", "app/components/Button", "app/components/ButtonRow"],
+(h, Button, ButtonRow) => {
 
-
-    //private extentiion of Button
-    //simply a button with a container that points to some content.
     class DisplayRowButton extends Button {
         constructor(label, content, container, componentStyles){
             super(label, container, componentStyles);
             this.content = content;
         }
     }
-    return class DisplayGridRow {
-        //Variables declared but not instantiated here are instantiated after render.
-        //items is an array of key value pairs
-        //container is an html element
+
+    return class DisplayGridRow extends ButtonRow {
         constructor(items, container, componentStyles){
-            let {buttonStyles, spacingStyle, displayStyle, componentStyle} = componentStyles;
-            this.buttonStyles =  buttonStyles;      //Styles passed down to buttons.
-            this.spacingStyle = spacingStyle;       //Style of container spacing divs where buttons are kept
-            this.displayStyle = displayStyle;       //Style of space where content is displayed
-            this.componentStyle = componentStyle;   //Style of top level grid CSS
+            super(items, container, DisplayRowButton, componentStyles);
+            //Style Objects
+            let {displayStyle} = componentStyles;
+            this.displayStyle = displayStyle;
 
-            this.container = container;
-            this.items = items;
-            //this.buttonSpacing is either undefined, and computed as a percentage of items on the rows
-            //or defined in order to space at a fixed length.
-            this.buttonSpacing = `${100/items.length}%`;
-            this.id = container.id;//dunno if we wanna do this
+            //Data Objects
+            //Inherited from ButtonRow
 
-            this.component;             //the whole html element
+            //JS Component Objects
+            //Inherited from ButtonRow
+
+            //HTML Component Objects
             this.display = "";          //the display html element
-            this.buttonRowContainer;    //the div of button elements
-            this.buttons = [];          //the button class objects
+
+            //State Objects
+            //Inherited from ButtonRow
         }
 
         activateOrHideDisplay(contentToDisplay){
@@ -44,25 +38,6 @@ define(["app/util/HTMLFragmentBuilder", "app/components/Button"],
                 this.deactivateAllButtons();
             }
             else this.setDisplay(contentToDisplay);
-        }
-
-        deactivateAllButtons(){
-            this.buttons.forEach(button => button.deactivate());
-        }
-
-        getActiveButton(){
-            return this.buttons.filter(button => button.active == true)[0];
-        }
-
-        buildDisplayGridButton(buttonText, buttonContent){
-            //overwrite whatever width spacing might be involved for whatever reason for the spacing attr
-            let spacingStyle = Object.assign(this.spacingStyle,{width:this.buttonSpacing});
-            let spacingContainer = h.div({
-                className: "button-spacing-container",
-                style: spacingStyle
-            });
-            let button = new DisplayRowButton(buttonText, buttonContent, spacingContainer, this.buttonStyles);
-            return button;
         }
 
         buildDisplay() {
@@ -89,33 +64,14 @@ define(["app/util/HTMLFragmentBuilder", "app/components/Button"],
             else this.display.innerHTML = content;
         }
 
-        buildRow(){
-            let buttonArray = [];
-            let rowItems = this.items;
-            rowItems.forEach(item => buttonArray.push(this.buildDisplayGridButton(item.key, item.value)));
-            return buttonArray;
-        }
-
         render(){
-            return new Promise(resolve => {
-                this.buttonRowContainer = h.div({
-                    className:"row-buttons"
+            return new Promise(resolve =>{
+                super.render().then(()=> {
+                    this.buttons.forEach((button)=> {
+                        button.addClickListener(()=>this.activateOrHideDisplay(button.content));
+                    });
+                    resolve(this);
                 });
-                this.component = h.div({
-                    className:"DisplayGridRow",
-                    style: this.componentStyle
-                },
-                    this.buttonRowContainer
-                );
-                this.buttons = this.buildRow();
-                this.buttons.forEach((button) => {
-                    this.buttonRowContainer.appendChild(button.container);
-                    button.render();
-                    button.addClickListener(()=>this.activateOrHideDisplay(button.content));
-                });
-                this.container.innerHTML = "";
-                this.container.appendChild(this.component);
-                resolve(this);
             });
         }
     };
