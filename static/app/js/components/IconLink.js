0edcc4f5ef153fefc92e4ae914cc4c13d9ca00f0
Frank Camilleri <fcamilleri22@gmail.com>
2017-04-16 19:21:51 -0400

    Added Hovers

--- static/app/js/components/IconLink.js
+++ static/app/js/components/IconLink.js
@@ -2,23 +2,38 @@ define(["app/util/HTMLFragmentBuilder"],
 (h) => {
     //Requires Bootstrap/FontAwesome
     return class IconLink{
-        constructor(fontAwesomeIconName, link, container, componentStyle, isDownload){
+        constructor(fontAwesomeIconName, link, container, isDownload){
             this.icon = fontAwesomeIconName;
             this.container = container;
             this.link = link;
             this.component;
             this.mouseover = false;
             this.isDownload = isDownload;
+            this.componentStyle = {color:"#33aadd"};
+            this.hoverStyle = {color:"#ff7e2a"};
+        }
+
+        applyStyle(newStyle){
+            Object.assign(this.component.style, newStyle);
         }
 
         render(){
+            const thisClass = this;
             let iconClass = `fa fa-${this.icon} fa-2x`;
             this.component =
             h.a({
                 href: this.link,
                 style:{
-                    color:"#33aadd"
-                }
+                    color:"#33aadd",
+                },
+                onmouseover:    () => {
+                    this.mouseover = true;
+                    this.applyStyle(thisClass.hoverStyle);
+                },
+                onmouseleave:   () => {
+                    this.mouseover = false;
+                    this.applyStyle(thisClass.componentStyle);
+                },
             },
                 h.i({
                     className: iconClass
